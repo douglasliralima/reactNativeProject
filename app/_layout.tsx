1573a169cfea5a617ca87812be84ec6a92d74d36
Douglas Lima <douglas.Lima@AB-inbev.com>
2024-08-31 21:50:39 -0300

    Added ESLint and prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,14 @@
-import { Text } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import { Text } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
-
   // I could just used the Slot here, it going to return the index component
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,23 @@
-import { Link } from 'expo-router';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
-
+import { Link } from "expo-router";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
       <StatusBar />
-      <Link href='/profile' style={{ color:'blue' }}>Profile</Link>
+      <Link href="/profile" style={{ color: "blue" }}>
+        Profile
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
