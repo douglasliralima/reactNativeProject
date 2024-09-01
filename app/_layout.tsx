36890fb4e79564e9d34f4a4c45ca2480c29ae195
Douglas Lima <douglas.Lima@AB-inbev.com>
2024-08-31 21:14:41 -0300

    Hello world with navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
 
+const RootLayout = () => {
 
-export default function RootLayout() {
+  // I could just used the Slot here, it going to return the index component
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
+
+export default RootLayout
